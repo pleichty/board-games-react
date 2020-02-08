54c1ad8c3b75804b32184b34b3f312ba7d0ffe2a
pleichty <pleichty@gmail.com>
2020-02-08 10:06:39 -0600

    using state/effect hooks

--- src/Components/BoardGame.js
+++ src/Components/BoardGame.js
@@ -1,39 +1,34 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import Axios from 'axios'
 import { BGG_API_BASE_URL } from '../constants/api'
 import { parseString } from 'xml2js'
 import { htmlDecode } from '../helpers/formatting'
 
-export class BoardGame extends Component {
-    state = {
-        thumbnail: '',
-        image: '',
+export default function BoardGame(props) {
+    const [bggPayload, setBggPayload] = useState({
         name: [],
+        thumbnail: '',
         description: ''
-    }
+    });
 
-    componentDidMount() {
-        Axios.get(`${BGG_API_BASE_URL}/thing?id=${this.props.id}`)
+    useEffect(() => {
+        Axios.get(`${BGG_API_BASE_URL}/thing?id=${props.id}`)
         .then(res => {
             let jsonPayload = {};
             parseString(res.data, {trim: true}, (err, result) => jsonPayload = result.items.item[0]);
-            this.setState({
-                ...this.state,
+            setBggPayload({
                 ...jsonPayload,
                 name: Object.values(jsonPayload.name[0])[0].value
             })
         })
-    }
+    });
 
-    render() {
         return (
             <div>
-                <span>{this.state.name}</span>
-                <img src={this.state.thumbnail} alt='cannot load'/>
-                <p>{htmlDecode(this.state.description)}</p>
+                <span>{bggPayload.name}</span>
+                <img src={bggPayload.thumbnail} alt='cannot load'/>
+                <p>{htmlDecode(bggPayload.description)}</p>
             </div>
         )
-    }
 }
 
-export default BoardGame
