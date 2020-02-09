2ba8f16e80c9b4936f0299b3098905f99d8c3a74
pleichty <pleichty@gmail.com>
2020-02-09 09:48:02 -0600

    changing to hooks

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -1,57 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component, useState} from 'react'
 import axios from 'axios'
 import { BGG_API_BASE_URL } from '../constants/api'
 import Suggestions from './Suggestions'
 import { parseString } from 'xml2js'
 
-class Search extends Component {
- state = {
-   query: '',
-   results: []
- }
-
- getInfo = () => {
-  axios.get(`${BGG_API_BASE_URL}/search?query=${this.state.query}`)
+export default function Search(props) {
+ const [query, setQuery] = useState('');
+ const [results, setResults] = useState([]);
+ 
+ const getInfo = () => {
+  axios.get(`${BGG_API_BASE_URL}/search?query=${query}`)
     .then(res => {
       let jsonPayload = {};
 
       parseString(res.data, {trim: true}, (err, result) => jsonPayload = result.items);
-      // setBggPayload({
-      //     ...jsonPayload,
-      //     name: Object.values(jsonPayload.name[0])[0].value
-      // })
-
-      this.setState({
-        results: jsonPayload.item
-      })
-      
-    })
+      setResults(jsonPayload.item);  
+    });
 }
 
-handleInputChange = () => {
-  this.setState({
-    query: this.search.value
-  }, () => {
-    if (this.state.query && this.state.query.length > 4) {
-      if (this.state.query.length % 2 === 0) {
-        this.getInfo()
-      }
+const handleInputChange = (event) => {
+  setQuery(event.target.value);
+    if (query && query.length > 3) {
+        getInfo()
     } 
-  })
 }
 
-render() {
   return (
     <form>
       <input
         placeholder="Search for..."
-        ref={input => this.search = input}
-        onChange={this.handleInputChange}
+        value={query}
+        onChange={handleInputChange}
       />
-      <Suggestions results={this.state.results} />
+      <Suggestions results={results} />
     </form>
   )
 }
-}
-
-export default Search
